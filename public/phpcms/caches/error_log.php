ce3df0ddb84eecb7ef09a14cbda33090183174b5
四月 <dancefun@163.com>
2017-09-14 21:08:57 +0800

    phpcms

---
+++ public/phpcms/caches/error_log.php
@@ -0,0 +1,12 @@
+<?php exit;?>09-14 20:41:02 | 2 | mysqli::mysqli(): (HY000/1049): Unknown database 'kodaiche' | phpcms\libs\classes\db_mysqli.class.php | 55
+<?php exit;?>09-14 20:41:02 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>09-14 20:41:02 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>09-14 20:41:02 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>09-14 20:41:02 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>09-14 20:41:02 | 2 | mysqli::close(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 409
+<?php exit;?>09-14 20:44:40 | 2 | mysqli::mysqli(): (HY000/1049): Unknown database 'kodaiche' | phpcms\libs\classes\db_mysqli.class.php | 55
+<?php exit;?>09-14 20:44:40 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>09-14 20:44:40 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>09-14 20:44:40 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>09-14 20:44:40 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>09-14 20:44:40 | 2 | mysqli::close(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 409
